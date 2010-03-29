a4a8fbb572282540b35a090efe5d2f9f178e6b6e
Geoff Salmon <geoff.salmon@gmail.com>
2010-03-29 14:57:56 -0400

    adds functions to stop and start a querier thread. Also experiments with using swing and ListModels

--- src/bonjure/querier.clj
+++ src/bonjure/querier.clj
@@ -1,16 +1,120 @@
 (ns bonjure.querier
+  (:import [java.util.concurrent DelayQueue Delayed TimeUnit])
   (:use [clojure.contrib.def :only [defvar defvar-]])
   )
 
-(defvar- data {})
-
 (defvar- listeners (atom (set [])) "Currently registered listener callbacks")
+(defvar- querier (atom nil))
+
+(defn get-data []
+  (let [data (:data @querier)]
+    (if (nil? data) nil @data)
+    )
+  )
+
+(defn queue-thread
+  "Create and start a thread to serve tasks from a given queue"
+  [queue]
+  (doto (Thread. (fn []
+                   (try
+                    (doseq [item (repeatedly #(.take queue)) :while (not (:shutdown item))]
+                      (try
+                       (println "Got item" item) (when-not (nil? (:action item)) ((:action item)))
+                       (catch Exception e
+                         (println "Exception while processing item" e)
+                         )))
+                    (finally (println "Finally Exiting.")))
+                   ))
+    (.start)
+    )
+  )
+
+(deftype Delayer [target action shutdown] java.util.concurrent.Delayed
+  (getDelay [unit]
+            (.convert unit (- target (System/currentTimeMillis)) TimeUnit/MILLISECONDS))
+  (compareTo [other] (println "compareTo" other) (- target (:target other))))
+
+
+(defn- build-delay [millis action shutdown]
+  (let [target (+ millis (System/currentTimeMillis))]
+    (Delayer target action shutdown))
+  )
+
+(defn push-task [millis action]
+  (let [q (:queue @querier)]
+    (when-not (nil? q)
+      (.add q (build-delay millis action false))
+      )
+    )
+  )
+
+(defn start
+  "Starts the querier if not already started. Does nothing if querier
+  is already running."
+  []
+  (swap! querier
+         (fn [old]
+           (if (nil? old)
+             ; start new querier
+             (let [d (atom {:rows []})
+                   q (java.util.concurrent.DelayQueue.)
+                   t (queue-thread q)
+                   ]
+
+               ; start data watcher
+               (add-watch d nil
+                          (fn [key r old new]
+                            (println "Watcher fired. notifying" (count @listeners) "listeners")
+                            (doseq [listener @listeners]
+                              (listener new))
+                            ))               
+               
+               (.add q (build-delay 0 #(println "First action!") false))
+               {
+                :queue q
+                :thread t
+                :data d
+                })
+             
+             ; querier already running!
+             old
+             )
+           ))
+  )
+
+(defn stop
+  "Stops the querier. Cleans up and stops background threads. Does
+  nothing if no querier is running"
+  []
+  (swap! querier
+         (fn [old]
+           (when-not (nil? old)
+             (remove-watch (:data old) nil)
+             (doto (:queue old)
+               ; clear all previous tasks and add a new task telling the
+               ; thread to exit
+               (.clear)
+               (.add (build-delay -1000 nil true)))
+             )
+           nil
+           ))
+  )
 
 (defn register-listener [listener]
-  (listener data) ; notify listener of current state
-  (swap! listeners conj listener)  
+  (listener (get-data)) ; notify listener of current state
+  (swap! listeners conj listener)
+  nil
   )
 
 (defn unregister-listener [listener]
   (swap! listeners disj listener)
+  nil
+  )
+
+
+(defn trigger []
+  (println "trigger")
+  (let [data (:data @querier)]
+    (when-not (nil? data)
+      (swap! data #(hash-map :rows (conj (:rows %) (str "Row " (count (:rows %))))))))
   )
--- src/bonjure/ui.clj
+++ src/bonjure/ui.clj
@@ -1,31 +1,61 @@
 (ns bonjure.ui
-  (:import (javax.swing JFrame JTable JLabel JTextField JButton)
-           (java.awt.event ActionListener WindowListener WindowAdapter WindowEvent)
+  (:import (javax.swing JFrame JPanel JTable JList JLabel JTextField JButton)
+           (javax.swing.event ListDataEvent)
+           (java.awt BorderLayout)
+           (java.awt.event WindowListener WindowAdapter WindowEvent)
            )
-
+  (:use [clojure.contrib.swing-utils :only [do-swing add-action-listener]])
   (:require bonjure.querier)
   )
 
+
+(defprotocol DataUI "UI Model that's interested in changes to data"
+  (data-updated [this]))
+
+(deftype ListModel [data latom]
+  :as this
+  javax.swing.ListModel
+  (addListDataListener [l] (swap! latom conj l))
+  (removeListDataListener [l] (swap! latom disj l))
+  (getElementAt [i] (nth (:rows @data) i))
+  (getSize [] (println "getSize" data @data (count (:rows @data))) (count (:rows @data)))
+  DataUI
+  (data-updated
+   []
+   (println "other method")
+   (let [evt (ListDataEvent. this ListDataEvent/CONTENTS_CHANGED 0 (count (:rows @data)))]
+     (doseq [l @latom] (.contentsChanged l evt)))
+   )  
+  )
+
 (defn open-frame []
-  (let [frame (JFrame. "Bonjure")
-        table (JTable. 4 2)
-
-        callback (fn [data]
-                   (println "Data changed")
-                   )
+  (let [
+        ui-data (atom (bonjure.querier/get-data)) ; data that UI model currently sees
+        data-listener (fn [data]
+                        (do-swing (reset! ui-data data)))
+        frame (JFrame. "Bonjure")
+        model (ListModel ui-data (atom []))
+        jlist (JList. model)
+        button (JButton. "Go")
         ]
-
+    (println "ui-data" ui-data)
+    (println "Initial data" (:data model))
+    (data-updated model)
+    (add-watch ui-data model (fn [key r old new] (data-updated model)))
+    (add-action-listener button (fn [evt] (bonjure.querier/trigger)))
+    
     (doto frame
 ;      (.setLayout (GridLayout. 2 2 3 3))
-      (.add table)
+      (.add jlist BorderLayout/CENTER)
+      (.add button BorderLayout/SOUTH)
       (.setSize 500 300)
       (.addWindowListener
        (proxy [WindowAdapter] []
          (windowClosing [evt]
                         (println "Window Closing" evt)
-                        (bonjure.querier/unregister-listener callback))
+                        (bonjure.querier/unregister-listener data-listener))
          )
        )
       (.setVisible true))
-    (bonjure.querier/register-listener callback)))
+    (bonjure.querier/register-listener data-listener)))
 
